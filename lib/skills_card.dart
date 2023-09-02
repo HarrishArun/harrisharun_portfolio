3d9b569d1d662e1a4f43f5aa744fe24c20f8f617
HarrishArun <95520465+HarrishArun@users.noreply.github.com>
2023-09-02 23:11:59 +0530

    Create skills_card.dart

---
+++ lib/skills_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class skills_card extends StatelessWidget {
+  const skills_card({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      color: Colors.deepPurple,
+    );
+  }
+}
